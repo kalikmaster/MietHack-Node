16faa43d71478a90fcfb31e098ebb0c2bb86fd97
Ilya Savchenko <tempestasnatus@gmail.com>
2022-10-23 07:19:28 +0300

    added user tokens

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const CryptoJS = require('crypto-js');
 bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
@@ -17,7 +18,6 @@ app.get('/forms/:type/:id', async (req, res) => {
     //const query = { _id: ObjectId('63540e516dd4c687d64366f8') };
     const query = { title: req.params.type }
     const form = await forms.findOne(query);
-    console.log(form.fields)
     res.render('form', { form: form.fields })
 })
 
@@ -26,10 +26,22 @@ app.post('/validate', async  (req, res) => {
     res.header('Content-Type', 'application/json')
     if (validate(req.body.initData)) {
         const initData = new URLSearchParams(req.body.initData);
-        const user = initData.get('user')
+        const user = JSON.parse(initData.get('user'))
+        const id = user.id
+        const query = { user_id: id }
+        const result = sessions.findOne({query});
+        let redacting = false
+        const token = CryptoJS.SHA256(id.toString() + process.env.API_TOKEN).toString(CryptoJS.enc.Hex)
+        if (result._id) {
+            redacting = result.redacting
+        } else {
+            const session = { user_id: id, token: token, redacting: false }
+            await sessions.insertOne(session)
+        }
         res.send(JSON.stringify({
             success: true,
-            user: user
+            token: token,
+            redacting: redacting
         }))
     } else {
         res.send(JSON.stringify({ success: false }))
